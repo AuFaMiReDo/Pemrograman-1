95596ac0cefc163afbd87738b0ebec3760067f56
AuFaMiReDo <144782802+AuFaMiReDo@users.noreply.github.com>
2023-12-19 18:43:58 +0800

    Delete Modul_4 directory

--- Modul_4/Bahasa Python/Prak401_2310817210013_MuhammadAufaFitrianda.py
+++
@@ -1,8 +0,0 @@
-kelipatan = int(input("Masukkan bilangan kelipatan: "))
-simbol = input("Masukkan simbol yang akan digunakan: ")
-
-for i in range(1, 51):
-    if i % kelipatan == 0:
-        print(simbol, end=' ')
-    else:
-        print(i, end=' ')
