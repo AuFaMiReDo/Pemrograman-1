6f4c7266c0912525abdabdcf7029db041d9c5f79
AuFaMiReDo <144782802+AuFaMiReDo@users.noreply.github.com>
2023-12-19 18:44:17 +0800

    Delete Modul_1 directory

--- Modul_1/Python/PRAK109-2310817210013-Muhammad_Aufa_Fitrianda.py
+++
@@ -1,6 +0,0 @@
-pasukanYuZhong = 958.730
-pasukanAlter = 5
-yangDikalahkan = pasukanYuZhong / pasukanAlter
-print (f"Jumlah pasukan yang dibawa Yu Zhong = ?")
-print (f"Jumlah pahlawan = ? ")
-print (f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {yangDikalahkan} pasukan")
