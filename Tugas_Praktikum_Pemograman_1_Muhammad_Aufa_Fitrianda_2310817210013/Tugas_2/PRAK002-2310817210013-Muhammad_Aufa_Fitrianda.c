717c20046197f3e50094333af79c9714bc585719
AuFaMiReDo <144782802+AuFaMiReDo@users.noreply.github.com>
2023-12-19 18:42:27 +0800

    Delete Tugas_Praktikum_Pemograman_1_Muhammad_Aufa_Fitrianda_2310817210013/Tugas_2 directory

--- Tugas_Praktikum_Pemograman_1_Muhammad_Aufa_Fitrianda_2310817210013/Tugas_2/PRAK002-2310817210013-Muhammad_Aufa_Fitrianda.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf ("Selamat Pagi, Muhammad Aufa Fitrianda\nSelamat Siang, Muhammad Aufa Fitrianda\nSelamat Malam, Muhammad Aufa Fitrianda");
-    return 0;
-}
--- Tugas_Praktikum_Pemograman_1_Muhammad_Aufa_Fitrianda_2310817210013/Tugas_2/PRAK002-2310817210013-Muhammad_Aufa_Fitrianda.py
+++
@@ -1,5 +0,0 @@
-nama = "Muhammad Aufa Fitrianda."
-
-print(f"Selamat Pagi, {nama}")
-print(f"Selamat Siang, {nama}")
-print(f"Selamat Malam, {nama}")
